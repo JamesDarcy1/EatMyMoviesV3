7ae6b4d41b91b31f6d08d5d494444e8ad5353d5e
James Darcy <james.darcy@hotmail.co.uk>
2024-06-16 16:28:26 +0100

    Update movie of the week

--- EatMyMoviesSite/Controllers/HomeController.cs
+++ EatMyMoviesSite/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace EatMyMoviesSite.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var movieOfTheWeek = "Sideways";
+            var movieOfTheWeek = "Magnolia";
             var tmdbMovie = await _movieService.GetMovieByTitle(movieOfTheWeek);
             var imdbRating = await _movieService.GetImdbRating(tmdbMovie.Title);
 
