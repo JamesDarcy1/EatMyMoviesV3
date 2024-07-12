891f86f1498facc51a2454adb8f6bcd42e99485c
James Darcy <james.darcy@hotmail.co.uk>
2024-07-12 17:19:08 +0100

    Update button colour again

--- EatMyMoviesSite/Controllers/HomeController.cs
+++ EatMyMoviesSite/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace EatMyMoviesSite.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var movieOfTheWeek = "Stand by me";
+            var movieOfTheWeek = "Before Sunset";
             var tmdbMovie = await _movieService.GetMovieByTitle(movieOfTheWeek);
             var imdbRating = await _movieService.GetImdbRating(tmdbMovie.Title);
 
