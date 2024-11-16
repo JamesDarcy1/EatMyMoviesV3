147252e45c2ab86ed15817c6c772cd1c642e047a
James Darcy <james.darcy@hotmail.co.uk>
2024-11-16 18:35:11 +0000

    Update movie of the week to palm springs

--- EatMyMoviesSite/Controllers/HomeController.cs
+++ EatMyMoviesSite/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace EatMyMoviesSite.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var movieOfTheWeek = "Land";
+            var movieOfTheWeek = "Palm Springs";
             var tmdbMovie = await _movieService.GetMovieByTitle(movieOfTheWeek);
             var imdbRating = await _movieService.GetImdbRating(tmdbMovie.Title);
 
