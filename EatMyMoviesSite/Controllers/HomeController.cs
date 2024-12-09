2029bfdeffb51fe90fe19d7835a6d5b985230aca
James Darcy <james.darcy@hotmail.co.uk>
2024-12-09 12:33:16 +0000

    Update movie of the week to the worst person in the world

--- EatMyMoviesSite/Controllers/HomeController.cs
+++ EatMyMoviesSite/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace EatMyMoviesSite.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var movieOfTheWeek = "12th Fail";
+            var movieOfTheWeek = "The worst person in the world";
             var tmdbMovie = await _movieService.GetMovieByTitle(movieOfTheWeek);
             var imdbRating = await _movieService.GetImdbRating(tmdbMovie.Title);
 
